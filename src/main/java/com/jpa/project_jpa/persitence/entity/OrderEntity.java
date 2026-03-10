fdc5a5858fd9982b022b067c29c1ce44c6e5c3df
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2026-03-09 22:52:17 -0500

    create entities

---
+++ src/main/java/com/jpa/project_jpa/persitence/entity/OrderEntity.java
@@ -0,0 +1,45 @@
+package com.jpa.project_jpa.persitence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "pizza_order")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private String idCustomer;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(6,2)")
+    private Double total;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String method;
+
+    @Column(name = "additional_notes", length = 200)
+    private String additionalNotes;
+
+    //@OneToOne
+    //@JoinColumn(name = "id_customer", referencedColumnName = "id_customer", insertable = false, updatable = false)
+    //private CustomerEntity customer;
+
+    //@OneToMany(mappedBy = "order")
+   //private List<OrderItemEntity> items;
+}
