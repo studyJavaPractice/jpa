a556e397fa785509189dabd21f69deb63a5c5454
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2026-03-09 21:33:02 -0500

    create project

---
+++ src/test/java/com/jpa/project_jpa/ProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.project_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
